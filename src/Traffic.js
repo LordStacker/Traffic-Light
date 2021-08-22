9a89552f898c0865287020b08731892b1e1f8cfe
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-08-22 16:20:09 -0400

    act

--- src/Traffic.js
+++ src/Traffic.js
@@ -1,6 +1,13 @@
 import React,{ useState } from "react";
 
 const Traffic = () => {
+    const [onred, setRed] = useState();
+    const [onyellow, setYellow] = useState();
+    const [ongreen, setGreen] = useState();
+    const Changecolor = () => {
+
+    }
+
     return (
         <div>
             <div className="Traffic-top">
@@ -8,18 +15,18 @@ const Traffic = () => {
             </div>
             <div className="col-md-1 bg-dark mx-auto rounded">
                 <div className="row">
-                    <div className="col-md-1 rounded-circle">
-                        <button className="RedLight rounded-circle mx-auto mb-1 mt-1"></button>
+                    <div className="col-md-1">
+                        <button className="RedLight rounded-circle mx-auto mb-1 mt-1 " onClick={Changecolor}></button>
                     </div>
                  </div>
                 <div className="row">
-                    <div className="col-md-1 rounded-circle">
-                    <button className="YellowLight rounded-circle mx-auto mb-1 mt-1"></button>
+                    <div className="col-md-1">
+                    <button className="YellowLight rounded-circle mx-auto mb-1 mt-1" onClick={Changecolor}></button>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-1">
-                    <button className="GreenLight rounded-circle mx-auto mb-1 mt-1"></button>
+                    <button className="GreenLight rounded-circle mx-auto mb-1 mt-1" onClick={Changecolor}></button>
                     </div>
                 </div>
             </div>
