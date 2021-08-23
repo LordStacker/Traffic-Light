457bae06b02a6618dc9e168e2f37b8b31a4e0012
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-08-22 21:28:03 -0400

    final commit

--- src/Traffic.js
+++ src/Traffic.js
@@ -1,10 +1,25 @@
 import React,{ useState } from "react";
 
 const Traffic = () => {
-    const [onred, setRed] = useState();
-    const [onyellow, setYellow] = useState();
-    const [ongreen, setGreen] = useState();
-    const Changecolor = () => {
+    const [onred, setRed] = useState(false);
+    const [onyellow, setYellow] = useState(false);
+    const [ongreen, setGreen] = useState(false);
+    const Changecolor = (o) => {
+        if(o.target.id === "redlight"){
+            setRed(true);
+            setYellow(false);
+            setGreen(false);
+        }
+        else if(o.target.id === "yellowlight"){
+            setRed(false);
+            setYellow(true);
+            setGreen(false);
+        }
+        else if(o.target.id === "greenlight"){
+            setRed(false);
+            setYellow(false);
+            setGreen(true);
+        }
 
     }
 
@@ -16,17 +31,17 @@ const Traffic = () => {
             <div className="col-md-1 bg-dark mx-auto rounded">
                 <div className="row">
                     <div className="col-md-1">
-                        <button className="RedLight rounded-circle mx-auto mb-1 mt-1 " onClick={Changecolor}></button>
+                        <button id="redlight" className={`RedLight rounded-circle mx-auto mb-1 mt-1 ${onred === true ? "highlighter" : ""}`} onClick={Changecolor}></button>
                     </div>
                  </div>
                 <div className="row">
                     <div className="col-md-1">
-                    <button className="YellowLight rounded-circle mx-auto mb-1 mt-1" onClick={Changecolor}></button>
+                    <button id="yellowlight" className={`YellowLight rounded-circle mx-auto mb-1 mt-1 ${onyellow === true ? "highlighter" : ""}`} onClick={Changecolor}></button>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-1">
-                    <button className="GreenLight rounded-circle mx-auto mb-1 mt-1" onClick={Changecolor}></button>
+                    <button id="greenlight" className={`GreenLight rounded-circle mx-auto mb-1 mt-1 ${ongreen === true ? "highlighter" : ""}`} onClick={Changecolor}></button>
                     </div>
                 </div>
             </div>
